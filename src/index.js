3855a9f489381aac87233d6846222fba99df0b1d
grza-cyber <grzannantuoni@gmail.com>
2025-03-26 23:52:35 +0100

    Created index.js file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function formatDate() {
+let currentDay = new Date();   
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let day = days[currentDay.getDay()];
+let currentTime = currentDay.getHours();
+let minute = currentDay.getMinutes();
+let currentDate = document.querySelector("#time");
+currentDate.innerHTML = `${day}, ${currentTime}:${minute}`;
+}
+let today = document.querySelector("#time");
+today.addEventListener("load", formatDate());
