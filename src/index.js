287b0e34e2e6773611cf5053687ba822f1df6bc4
grza-cyber <grzannantuoni@gmail.com>
2025-03-26 23:59:03 +0100

    Added weatherInfo function plus link to axios

--- src/index.js
+++ src/index.js
@@ -15,5 +15,8 @@ let minute = currentDay.getMinutes();
 let currentDate = document.querySelector("#time");
 currentDate.innerHTML = `${day}, ${currentTime}:${minute}`;
 }
+function weatherInfo() {
+    
+}
 let today = document.querySelector("#time");
 today.addEventListener("load", formatDate());
