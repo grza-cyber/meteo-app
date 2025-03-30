212e23a9800660b0611f5673d95aa473a91769d8
grza-cyber <grzannantuoni@gmail.com>
2025-03-30 19:17:06 +0200

    Updated files and injected new data

--- src/index.js
+++ src/index.js
@@ -13,28 +13,59 @@ let day = days[currentDay.getDay()];
 let currentTime = currentDay.getHours();
 let minute = currentDay.getMinutes();
 let currentDate = document.querySelector("#time");
+if (minute < 10) {
+    minute = `0${minute}`;
+}
 currentDate.innerHTML = `${day}, ${currentTime}:${minute}`;
 }
 let today = document.querySelector("#time");
 today.addEventListener("load", formatDate());
 
-function currentTemperature(response) {
+function refreshMeteo(response) {
+
+
+let headerContent = document.querySelector("#city");
+headerContent.innerHTML = response.data.city;
+
+let descriptionElement = document.querySelector("#description");
+let description = response.data.condition.description;
+descriptionElement.innerHTML = `, ${description}`;
+
+let humidityElement = document.querySelector("#humidity");
+let humidity = response.data.temperature.humidity;
+humidityElement.innerHTML = `${humidity}%`;    
+
+let windElement = document.querySelector("#wind-speed");
+let windSpeed = response.data.wind.speed;
+windElement.innerHTML = `${windSpeed} km/h`;
+
 let temperatureElement = document.querySelector("#temperature");
 let currentTemperature = Math.round(response.data.temperature.current);
 temperatureElement.innerHTML = currentTemperature;
-let headerContent = document.querySelector("#city");
-headerContent.innerHTML = response.data.city;
+if (currentTemperature < 10) {
+    temperatureElement.innerHTML = `&nbsp;${currentTemperature}`;
 }
 
-function displayCity(event) {
-    event.preventDefault();
-    let searchInputElement = document.querySelector("#search-city");
-    let city = searchInputElement.value;
-    
+let iconElement = document.querySelector("#icon");
+let icon = response.data.condition.icon_url;
+iconElement.innerHTML = `<img src="${icon}" />`;
+
+console.log(response.data);
+}
+
+function searchCity(city) {
     let apiKey = "4a76f73ec0tb4cf9o9b3b1c325220356";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(currentTemperature);
+    axios.get(apiUrl).then(refreshMeteo);
 }
+
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInputElement = document.querySelector("#search-city");
+    searchCity(searchInputElement.value);
+}
+
 let formElement = document.querySelector("#searchForm");
-formElement.addEventListener("submit", displayCity);
+formElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Zürich");
